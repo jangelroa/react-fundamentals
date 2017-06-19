4681644f453ba67a8c3ddb981d5c0e08ef25a8fa
jangelroa <jangelroa@gmail.com>
2017-06-18 20:55:57 -0700

    Stateless_Functional_Components

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,31 @@
 var React = require("react");
+var PropTypes = require("prop-types");
+
+function SelectLanguage(props) {
+  var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+  return (
+    <ul className="languages">
+      {languages.map(function(lang) {
+        return (
+          <li
+            key={lang}
+            onClick={props.onSelect.bind(null, lang)}
+            style={
+              props.selectedLanguage === lang ? { color: "#d0021b" } : null
+            }
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component {
   constructor(props) {
@@ -16,25 +43,11 @@ class Popular extends React.Component {
     });
   }
   render() {
-    var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
     return (
-      <ul className="languages">
-        {languages.map(function(lang) {
-          return (
-            <li
-              key={lang}
-              onClick={this.updateLanguage.bind(null, lang)}
-              style={
-                this.state.selectedLanguage === lang
-                  ? { color: "#d0021b" }
-                  : null
-              }
-            >
-              {lang}
-            </li>
-          );
-        }, this)}
-      </ul>
+      <SelectLanguage
+        selectedLanguage={this.state.selectedLanguage}
+        onSelect={this.updateLanguage}
+      />
     );
   }
 }
